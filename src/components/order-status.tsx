5b3f28d4ee267c3e598a9105c0d05f96c9673d38
Rodrigo <rodrigo.brandao98@gmail.com>
2024-01-20 11:03:32 -0300

    class: Listagem de pedidos

---
+++ src/components/order-status.tsx
@@ -0,0 +1,36 @@
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface OrderStatusProps {
+  status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span
+        data-pending={status === 'pending'}
+        data-canceled={status === 'canceled'}
+        data-delivered={status === 'delivered'}
+        data-delivering={status === 'delivering'}
+        data-processing={status === 'processing'}
+        className="0 h-2 w-2 rounded-full data-[canceled=true]:bg-rose-500 data-[delivered=true]:bg-emerald-500 data-[delivering=true]:bg-amber-500 data-[pending=true]:bg-slate-400 data-[processing=true]:bg-amber-500"
+      />
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,12 +1,27 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
 
+import { OrderStatus } from '@/components/order-status'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
 
 import { OrderDetails } from './order-details'
 
-export function OrderTableRow() {
+interface OrderProps {
+  order: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }
+}
+
+export function OrderTableRow({
+  order: { orderId, createdAt, status, customerName, total },
+}: OrderProps) {
   return (
     <TableRow>
       <TableCell>
@@ -20,18 +35,23 @@ export function OrderTableRow() {
           <OrderDetails />
         </Dialog>
       </TableCell>
-      <TableCell className="font-mono text-sm font-medium">
-        ad7ga876d76das
+      <TableCell className="font-mono text-sm font-medium">{orderId}</TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(new Date(createdAt), {
+          locale: ptBR,
+          addSuffix: true,
+        })}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
+        <OrderStatus status={status} />
       </TableCell>
-      <TableCell className="font-medium">Rodrigo Brandão</TableCell>
-      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell className="font-medium">{customerName}</TableCell>
+      <TableCell className="font-medium">
+        {total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
