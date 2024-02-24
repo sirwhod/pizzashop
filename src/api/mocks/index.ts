b92fa17a48e1c378aef7bbe2a366d08b009e39ff
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-02-24 18:28:59 -0300

    class: Mock de perfil e atualização

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,14 +2,17 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { getManagedRestaurantMock } from './get--managed-restaurant-mock.ts'
 import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
 import { getDailyRevenueInPeriodMock } from './get-day-revenue-in-period-mock.ts'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock.ts'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock.ts'
 
 export const worker = setupWorker(
   signInMock,
@@ -20,6 +23,9 @@ export const worker = setupWorker(
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
 )
 
 export async function enableMSW() {
