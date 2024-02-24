c0cf2917a7bd766f7c24fb5cb4787971b76471ee
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-02-23 21:58:18 -0300

    class: Mocks de cadastro e dashboard

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,9 +2,25 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getDailyRevenueInPeriodMock } from './get-day-revenue-in-period-mock.ts'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
