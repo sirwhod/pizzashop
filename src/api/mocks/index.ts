2e91492bdf8dcb4366336cf4bd822e7f78b5a654
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-02-27 20:59:54 -0300

    class: Criando mocks de ações

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,19 +2,23 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { approveOrderMock } from './approve-order-mock.ts'
+import { cancelOrderMock } from './cancel-order-mock.ts'
+import { deliverOrderMock } from './deliver-order-mock.ts'
+import { dispatchOrderMock } from './dispatch-order-mock.ts'
 import { getManagedRestaurantMock } from './get--managed-restaurant-mock.ts'
 import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
 import { getDailyRevenueInPeriodMock } from './get-day-revenue-in-period-mock.ts'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getOrderDetailsMock } from './get-orders-details-mock.ts'
 import { getOrdersMock } from './get-orders-mock.ts'
 import { getPopularProductsMock } from './get-popular-products-mock'
 import { getProfileMock } from './get-profile-mock.ts'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 import { updateProfileMock } from './update-profile-mock.ts'
-import { getOrderDetailsMock } from './get-orders-details-mock.ts'
 
 export const worker = setupWorker(
   signInMock,
@@ -30,6 +34,10 @@ export const worker = setupWorker(
   updateProfileMock,
   getOrdersMock,
   getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 )
 
 export async function enableMSW() {
